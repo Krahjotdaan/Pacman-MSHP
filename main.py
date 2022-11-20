507f54cc3777c2619bd1701e2923ae9df6c84ac7
Leha <managestudylexa@gmail.com>
2022-11-20 20:13:10 +0300

    base main file pattern

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+import pygame
+
+class Settings:
+    BACKGROUND_COLOR = pygame.Color('black')
+    WINDOW_WIDTH = 800
+    WINDOW_HEIGHT = 600
+
 def main():
-    print(42)
+    pygame.init()
+    pygame.font.init()
+    screen = pygame.display.set_mode([Settings.WINDOW_WIDTH, Settings.WINDOW_HEIGHT])
+
+
+    # Основной цикл программы
+    game_over = False
+    while not game_over:
+        pass
+    exit(0)
+
 if __name__ == '__main__':
     main()
