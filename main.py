64025373d6dbe98ce6cba3bbf8ca005d44c90bea
Leha <managestudylexa@gmail.com>
2022-11-21 12:20:11 +0300

    packman base class

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ class Settings:
     BACKGROUND_COLOR = pygame.Color('black')
     WINDOW_WIDTH = 800
     WINDOW_HEIGHT = 600
-
+class Packman:
+    def __init__(self, pos, shift, filename):
+        self.pos = pos
+        self.shift = shift
+        self.image = pygame.image.load(filename)
+        self.rect = self.image.get_rect()
 def main():
     pygame.init()
     pygame.font.init()
