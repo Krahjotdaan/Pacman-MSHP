ff54d84da6a4ddc53d9330acf2386fd267301450
Lyuba <lublub.f@gmail.com>
2022-11-23 20:01:02 +0300

    ghost_start

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ def main():
     pygame.init()
     pygame.font.init()
     screen = pygame.display.set_mode([Settings.WINDOW_WIDTH, Settings.WINDOW_HEIGHT])
-
+    ghost_1 = Ghost((800, 0),(0, 1), "ghost.png")
 
     # Основной цикл программы
     game_over = False
     while not game_over:
-        pass
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
     exit(0)
 
 if __name__ == '__main__':
