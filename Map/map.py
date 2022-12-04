b28f0a3b25740e2dbae316959f2a5a823ace1ff1
Igor <orl@gmail.com>
2022-12-04 19:22:54 +0300

    Version 1.3

--- Map/map.py
+++ Map/map.py
@@ -75,23 +75,23 @@ def visualizeGrid():
     pygame.display.update()
 
 
-def main():
-    pygame.init()
-    pygame.font.init()
-    screen = pygame.display.set_mode([Settings.WINDOW_WIDTH, Settings.WINDOW_HEIGHT])
-
-    # Основной цикл программы
-    game_over = False
-    while not game_over:
-        # обработка событий
-        for event in pygame.event.get():
-            game_over = check_for_exit(event)
-        screen.fill(Settings.BACKGROUND_COLOR)
-        visualizeGrid()
-        pygame.display.flip()
-    exit(0)
-def check_for_exit(event):
-    return event.type == pygame.QUIT
-
-if __name__ == '__main__':
-    main()
+# def main():
+#     pygame.init()
+#     pygame.font.init()
+#     screen = pygame.display.set_mode([Settings.WINDOW_WIDTH, Settings.WINDOW_HEIGHT])
+#
+#     # Основной цикл программы
+#     game_over = False
+#     while not game_over:
+#         # обработка событий
+#         for event in pygame.event.get():
+#             game_over = check_for_exit(event)
+#         screen.fill(Settings.BACKGROUND_COLOR)
+#         visualizeGrid()
+#         pygame.display.flip()
+#     exit(0)
+# def check_for_exit(event):
+#     return event.type == pygame.QUIT
+#
+# if __name__ == '__main__':
+#     main()
