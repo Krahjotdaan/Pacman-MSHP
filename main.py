f9b1d098204d9a51e6657e749cdbb27695258763
Ivan Demin <findemin@gmail.com>
2022-12-11 18:42:25 +0300

    New button for records + updated menu

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ def set_scene(index):
 
 def set_game_scene():
     set_scene(1)
+def set_menu_scene():
+    set_scene(0)
+def set_records_scene():
+    set_scene(3)
 
 background_color = pygame.Color('black')
 
@@ -31,7 +35,12 @@ def main():
     # Инициализация сцены 0 (menu)
     global event
     pygame.font.init()
-    scene_0_button_new_geometry = pygame.Rect(Settings.WINDOW_WIDTH / 2 - 100 / 2, Settings.WINDOW_HEIGHT / 2 - 10 - 50, 100, 50)
+    scene_0_button_records_geometry = pygame.Rect(Settings.WINDOW_WIDTH / 2 - 100 / 2, Settings.WINDOW_HEIGHT / 2 - 10 - 50,
+                                              100, 50)
+    scene_0_button_back_geometry = pygame.Rect(Settings.WINDOW_WIDTH / 2 - 100 / 2,
+                                                  Settings.WINDOW_HEIGHT / 2 + 300 - 50,
+                                                  100, 50)
+    scene_0_button_new_geometry = pygame.Rect(Settings.WINDOW_WIDTH / 2 - 100 / 2, Settings.WINDOW_HEIGHT / 2 - 80 - 50, 100, 50)
     scene_0_button_exit_geometry = pygame.Rect(Settings.WINDOW_WIDTH / 2 - 100 / 2, Settings.WINDOW_HEIGHT / 2 + 10, 100, 50)
 
     BUTTON_STYLE = {
@@ -41,6 +50,10 @@ def main():
         "hover_font_color": pygame.Color('orange'),
     }
 
+    records_button = Button(scene_0_button_records_geometry, pygame.Color('gray'), set_records_scene,
+                             text='Records', **BUTTON_STYLE)
+    back_button = Button(scene_0_button_back_geometry, pygame.Color('gray'), set_menu_scene,
+                            text='Back', **BUTTON_STYLE)
     new_game_button = Button(scene_0_button_new_geometry, pygame.Color('gray'), set_game_scene,
                              text='New game', **BUTTON_STYLE)
     exit_button = Button(scene_0_button_exit_geometry, pygame.Color('gray'), exit,
@@ -57,6 +70,8 @@ def main():
             if event.type == pygame.QUIT:
                 game_over = True
             if Settings2.scene_index == 0:  # menu
+                records_button.check_event(event)
+                back_button.check_event(event)
                 new_game_button.check_event(event)
                 exit_button.check_event(event)
             elif Settings2.scene_index == 1:  # game
@@ -72,12 +87,17 @@ def main():
             screen.fill(background_color)
             if Settings2.scene_index == 0:  # menu
                 # четыре анимированные линии (две кнопки уже отрисовались)
+                records_button.update(screen)
                 new_game_button.update(screen)
                 exit_button.update(screen)
             elif Settings2.scene_index == 1:  # game
                 pass #сюда пишется отрисовка всей игры
             elif Settings2.scene_index == 2:  # gameover
                 pass
+            elif Settings2.scene_index == 3:
+                back_button.update(screen)
+                back_button.check_event(event)
+                pygame.draw.rect(screen, (0,0,255), [Settings.WINDOW_WIDTH//2-250,Settings.WINDOW_HEIGHT//6,500,500])
             pygame.display.flip()
     exit(0)
 
