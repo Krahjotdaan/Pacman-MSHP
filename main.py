f49c5860e61e6df5d6efe675076c7df4d7ed6ec8
Orlov Igor <ioproglab@gmail.com>
2022-12-12 00:28:11 +0300

    demo

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import pygame
 
-#shfsjkh
 from settings import Settings
 from Map.map import Map
-from Map.map import visualizeGrid
 from Packman.packman import Packman
 from Music.music import playMusic
 
@@ -11,29 +9,36 @@ my_map = Map()
 
 gridDisplay = pygame.display.set_mode((1200, 900))
 pygame.display.get_surface().fill((200, 200, 200))  # background
-packman = Packman([30,120], [0,0], "Packman/pacmanOpen.png")
+packman = Packman([30, 120], [0, 0], "Packman/pacmanOpen.png")
 playMusic('Music/pacman_music.mp3')
-
 
 def main():
     # Основной цикл программы
     game_over = False
+    score = 0
     while not game_over:
         # обработка событий
         for event in pygame.event.get():
             game_over = check_for_exit(event)
         screen = pygame.display.set_mode([Settings.WINDOW_WIDTH, Settings.WINDOW_HEIGHT])
-        visualizeGrid()
-        # for sd in my_map.seeds:
-        #     if packman.get_position()[0] + 15 == sd.pos[0] and packman.get_position()[1] + 15 == sd.pos[1]:
-        #         my_map.seeds.remove(sd)
-        #         sd.color = (0, 0, 0)
-        #         sd.draw(screen)
+
+        my_map.visualizeGrid()
+        my_map.draw_seeds()
+
+        for sd in my_map.seeds: # проверка того, что пакман съедает семку
+            if packman.get_position()[0] + 15 == sd.pos[0] and packman.get_position()[1] + 15 == sd.pos[1]:
+                if sd.is_super == True:
+                    my_map.seeds.remove(sd)
+                    score += 5 # начисление очков за большую семку
+                else:
+                    my_map.seeds.remove(sd)
+                    score += 1 # начисление очков за обычную семку
+
+
         packman.draw(screen)
         packman.event(event)
         packman.move()
         pygame.display.flip()
-
     exit(0)
 
 def check_for_exit(event):
